1623089966aae38a1b036a11965d5369f630537a
UcheIgbokwe <uchehenryigbokwe@gmail.com>
2017-10-03 19:00:10 +0100

    features: commit modules,config,migration

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Recipe, {
+      foreignKey: 'userId',
+      as: 'recipes',
+    });
+  };
+
+  return User;
+};
