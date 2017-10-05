b751082c7b31c3de03b2639d9cbb33fa8a7b02c6
UcheIgbokwe <uchehenryigbokwe@gmail.com>
2017-10-05 19:07:41 +0100

    chore: commit heroku changes

--- server/config/config.js
+++ server/config/config.js
@@ -11,14 +11,6 @@ module.exports = {
     dialect: 'postgres'
   },
   test: {
-      username: 'root',
-      password: null,
-      database: 'database_test',
-      host: '127.0.0.1',
-      dialect: 'mysql',
-    logging: false
-  },
-  production: {
     username: 'postgres',
     password: 'Ebubechi',
     database: 'MoreRecipe',
--- server/controlllers/favorites.js
+++ server/controlllers/favorites.js
@@ -1,116 +1,63 @@
-import models from '../models';
+import db from '../models/';
 
-const recipe = models.Recipe;
-const user = models.User;
-const review = models.Review;
-const favourite = models.Favourite;
+const Favorites = db.Favorites;
+const Recipes = db.Recipes;
 
-/**
- * 
- * 
- * @export
- * @class Favourite
- */
-export default class Favourite {
+const favoritesController = {
   /**
-   * 
-   * 
-   * @param {any} req 
-   * @param {any} res 
-   * @returns 
-   * @memberof Favourite
+   * Favorite a recipe
+   *
+   * @param {any} request
+   * @param {any} response
+   * @returns {obj} obj
    */
-  addFavourite(req, res) {
-    if (!(req.params.recipeId)) {
-      return res.status(400)
-        .send('Include ID of recipe to favourite');
-    }
-    if (isNaN(req.params.recipeId)) {
-      return res.status(400)
-        .send('Invalid recipeId. recipeId should be a number');
-    }
-    recipe.findById(req.params.recipeId)
-      .then((found) => {
-        if (!found) {
-          return res.status(404)
-            .send('recipe doesn\'t exist in catalogue');
-        }
-        favourite.findOne({
-          where: {
-            recipeId: req.params.recipeId,
-            $and: {
-              userId: req.decoded.id
-            }
-          }
-        })
-          .then((foundRecipe) => {
-            if (foundRecipe) {
-              return res.status(403)
-                .json({
-                  status: 'Fail',
-                  message: 'Already added this recipe to your favourites'
-                });
-            }
-            favourite.create({
-              userId: req.decoded.id,
-              recipeId: req.params.recipeId
-            })
-              .then(() => res.status(201)
-                  .json({
-                    status: 'Success',
-                    message: 'Recipe added to favourites'
-                  }))
-              .catch(() => res.status(500)
-                  .send('Unable to add to favourites due to server error'));
-          })
-          .catch(() => res.status(500)
-              .send('a server error ocurred'));
-      })
-      .catch(() => res.status(500)
-          .send('Internal server error, please try again later'));
 
-    return this;
-  }
-  /**
-   * 
-   * 
-   * @param {any} req 
-   * @param {any} res 
-   * @returns 
-   * @memberof Favourite
-   */
-  getAll(req, res) {
-    favourite.findAll({
+  create(request, response) {
+    if (!request.params.id) {
+      return response.status(404).json({ error: 'Recipe id is required.' });
+    }
+    Favorites.findOne({
       where: {
-        userId: req.params.userId
-      },
-      include: [
-        {
-          model: recipe, attributes: ['name', 'ingredients', 'directions']
-        }
-      ]
+        recipeId: request.params.id,
+        userId: request.decoded.id
+      }
     })
-      .then((found) => {
-        if (!found) {
-          return res.status(404)
-            .json({
-              status: 'success',
-              message: 'You have no recipes added as favourites'
-            });
+      .then((favorite) => {
+        if (favorite) {
+          return response.status(400).json({
+            message: 'Recipe already favorited.'
+          });
         }
-        if (found) {
-          return res.status(200)
-            .json({
-              status: 'Success',
-              favourites: found
-            });
+        return Favorites.create({
+          recipeId: request.params.id,
+          userId: request.decoded.id
+        });
+      })
+      .then(favoriteSuccess => response.status(201).json({
+        message: 'Recipe successfully favorited.',
+        data: favoriteSuccess
+      }))
+      .catch(error => response.status(400).json({
+        message: 'An error occured during this operation',
+        error: error.message
+      }));
+  },
+
+  getAll(request, response) {
+    Favorites.findAll({
+      where: { userId: request.params.id },
+      include: [{ model: Recipes }]
+    })
+      .then((isFound) => {
+        if (isFound.length === 0) {
+          return response.status(404).json({ message: 'No favorites found.' });
+        } else
+        if (isFound) {
+          return response.status(200).json({ message: isFound });
         }
       })
-      .catch((error) => {
-        console.log(error);
-        return res.status(500)
-          .send('Unable to get favourites, internal server error');
-      });
-    return this;
+      .catch(error => response.status(400).json({ error: error.message }));
   }
-}
+};
+
+export default favoritesController;
